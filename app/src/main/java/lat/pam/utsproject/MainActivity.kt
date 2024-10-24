4fc0d1c68fe3c2db489e5e38811a0438219afeda
Alyanrllff <lathifahalyanurul@gmail.com>
2024-10-24 16:36:33 +0700

    Initial commit

---
+++ app/src/main/java/lat/pam/utsproject/MainActivity.kt
@@ -0,0 +1,32 @@
+package lat.pam.utsproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Referensi ke button login
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            // Intent untuk berpindah ke ListFoodActivity
+            val intent = Intent(this, ListFoodActivity::class.java)
+            startActivity(intent)
+        }
+}
+}
